fe632987a2ac900b82cbf50c84ea523a71bda90a
Mihai Mureșan <mihaimuresan592003@gmail.com>
2024-12-23 08:16:12 +0200

    fix: create environments directory if non-existent

--- src/scripts/set-env.js
+++ src/scripts/set-env.js
@@ -1,13 +1,19 @@
-const { writeFileSync } = require("fs");
+const { writeFileSync, mkdirSync } = require("fs");
 const { resolve } = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const envPath = resolve(__dirname, "../environments/environment.ts");
+const envDir = resolve(__dirname, "../environments");
+mkdirSync(envDir, { recursive: true });
+
+const envPath = resolve(envDir, "environment.ts");
+const envDevPath = resolve(envDir, "environment.development.ts");
+
 const envContent = `export const environment = {
   backendUrl: '${process.env.BACKEND_URL}',
 };
 `;
 
 writeFileSync(envPath, envContent);
+writeFileSync(envDevPath, envContent);
